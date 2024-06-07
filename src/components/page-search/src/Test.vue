7b1f95c2a749a32f730a4978aa7c3b562b696a92
unknown <775709548@qq.com>
2024-06-07 14:38:21 +0800

    vue+ts

---
+++ src/components/page-search/src/Test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="">
+
+  </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
+
+
+<style lang="less" scoped>
+
+</style>
